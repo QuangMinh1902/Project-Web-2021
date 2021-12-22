df7b288c62470dcc3570c146e7bc1b03d2e9bf7c
QUANG MINH <quangminh1575@gmail.com>
2021-12-22 16:57:03 +0100

    updated

--- database/seeders/PlanningSeeder.php
+++ database/seeders/PlanningSeeder.php
@@ -14,49 +14,189 @@ class PlanningSeeder extends Seeder
     public function run()
     {
         DB::table('plannings')->insert([
-            'cours_id' => '1',
+            'cours_id' => '7',
             'date_debut' => date("Y-m-d h:i:s"),
             'date_fin' => date("2021-05-12 07:12:54"),
         ]);
         DB::table('plannings')->insert([
-            'cours_id' => '2',
+            'cours_id' => '7',
             'date_debut' => date("Y-m-d h:i:s"),
             'date_fin' => date("2021-06-12 07:12:54"),
         ]);
         DB::table('plannings')->insert([
-            'cours_id' => '3',
+            'cours_id' => '8',
             'date_debut' => date("Y-m-d h:i:s"),
             'date_fin' => date("2021-07-16 07:12:54"),
         ]);
         DB::table('plannings')->insert([
-            'cours_id' => '1',
+            'cours_id' => '8',
             'date_debut' => date("Y-m-d h:i:s"),
             'date_fin' => date("2021-06-22 07:12:54"),
         ]);
         DB::table('plannings')->insert([
-            'cours_id' => '6',
+            'cours_id' => '9',
             'date_debut' => date("Y-m-d h:i:s"),
             'date_fin' => date("2021-012-21 07:12:54"),
         ]);
         DB::table('plannings')->insert([
-            'cours_id' => '7',
+            'cours_id' => '9',
             'date_debut' => date("Y-m-d h:i:s"),
             'date_fin' => date("2021-10-01 07:12:54"),
         ]);
         DB::table('plannings')->insert([
-            'cours_id' => '8',
+            'cours_id' => '9',
             'date_debut' => date("Y-m-d h:i:s"),
             'date_fin' => date("2021-11-02 07:12:54"),
         ]);
         DB::table('plannings')->insert([
-            'cours_id' => '11',
+            'cours_id' => '10',
             'date_debut' => date("Y-m-d h:i:s"),
             'date_fin' => date("2021-12-25 07:12:54"),
         ]);
         DB::table('plannings')->insert([
-            'cours_id' => '9',
+            'cours_id' => '10',
             'date_debut' => date("Y-m-d h:i:s"),
             'date_fin' => date("2021-12-11 07:12:54"),
+        ]);
+        DB::table('plannings')->insert([
+            'cours_id' => '11',
+            'date_debut' => date("Y-m-d h:i:s"),
+            'date_fin' => date("2021-7-21 07:12:54"),
+        ]);
+        DB::table('plannings')->insert([
+            'cours_id' => '11',
+            'date_debut' => date("Y-m-d h:i:s"),
+            'date_fin' => date("2021-7-21 07:12:54"),
+        ]);
+        DB::table('plannings')->insert([
+            'cours_id' => '1',
+            'date_debut' => date("Y-m-d h:i:s"),
+            'date_fin' => date("2021-7-21 07:12:54"),
+        ]);
+        DB::table('plannings')->insert([
+            'cours_id' => '2',
+            'date_debut' => date("Y-m-d h:i:s"),
+            'date_fin' => date("2021-7-21 07:12:54"),
+        ]);
+        DB::table('plannings')->insert([
+            'cours_id' => '12',
+            'date_debut' => date("Y-m-d h:i:s"),
+            'date_fin' => date("2021-7-21 07:12:54"),
+        ]);
+        DB::table('plannings')->insert([
+            'cours_id' => '12',
+            'date_debut' => date("Y-m-d h:i:s"),
+            'date_fin' => date("2021-7-21 07:12:54"),
+        ]);
+        DB::table('plannings')->insert([
+            'cours_id' => '13',
+            'date_debut' => date("Y-m-d h:i:s"),
+            'date_fin' => date("2021-7-21 07:12:54"),
+        ]);
+        DB::table('plannings')->insert([
+            'cours_id' => '13',
+            'date_debut' => date("Y-m-d h:i:s"),
+            'date_fin' => date("2021-7-21 07:12:54"),
+        ]);
+        DB::table('plannings')->insert([
+            'cours_id' => '13',
+            'date_debut' => date("Y-m-d h:i:s"),
+            'date_fin' => date("2021-7-21 07:12:54"),
+        ]);
+        DB::table('plannings')->insert([
+            'cours_id' => '14',
+            'date_debut' => date("Y-m-d h:i:s"),
+            'date_fin' => date("2021-7-21 07:12:54"),
+        ]);
+        DB::table('plannings')->insert([
+            'cours_id' => '14',
+            'date_debut' => date("Y-m-d h:i:s"),
+            'date_fin' => date("2021-7-21 07:12:54"),
+        ]);
+        DB::table('plannings')->insert([
+            'cours_id' => '15',
+            'date_debut' => date("Y-m-d h:i:s"),
+            'date_fin' => date("2021-7-21 07:12:54"),
+        ]);
+        DB::table('plannings')->insert([
+            'cours_id' => '15',
+            'date_debut' => date("Y-m-d h:i:s"),
+            'date_fin' => date("2021-7-21 07:12:54"),
+        ]);
+        DB::table('plannings')->insert([
+            'cours_id' => '16',
+            'date_debut' => date("Y-m-d h:i:s"),
+            'date_fin' => date("2021-7-21 07:12:54"),
+        ]);
+        DB::table('plannings')->insert([
+            'cours_id' => '17',
+            'date_debut' => date("Y-m-d h:i:s"),
+            'date_fin' => date("2021-7-21 07:12:54"),
+        ]);
+        DB::table('plannings')->insert([
+            'cours_id' => '18',
+            'date_debut' => date("Y-m-d h:i:s"),
+            'date_fin' => date("2021-7-21 07:12:54"),
+        ]);
+        DB::table('plannings')->insert([
+            'cours_id' => '19',
+            'date_debut' => date("Y-m-d h:i:s"),
+            'date_fin' => date("2021-7-21 07:12:54"),
+        ]);
+        DB::table('plannings')->insert([
+            'cours_id' => '20',
+            'date_debut' => date("Y-m-d h:i:s"),
+            'date_fin' => date("2021-7-21 07:12:54"),
+        ]);
+        DB::table('plannings')->insert([
+            'cours_id' => '20',
+            'date_debut' => date("Y-m-d h:i:s"),
+            'date_fin' => date("2021-7-21 07:12:54"),
+        ]);
+        DB::table('plannings')->insert([
+            'cours_id' => '20',
+            'date_debut' => date("Y-m-d h:i:s"),
+            'date_fin' => date("2021-7-21 07:12:54"),
+        ]);
+        DB::table('plannings')->insert([
+            'cours_id' => '18',
+            'date_debut' => date("Y-m-d h:i:s"),
+            'date_fin' => date("2021-7-21 07:12:54"),
+        ]);
+        DB::table('plannings')->insert([
+            'cours_id' => '17',
+            'date_debut' => date("Y-m-d h:i:s"),
+            'date_fin' => date("2021-7-21 07:12:54"),
+        ]);
+        DB::table('plannings')->insert([
+            'cours_id' => '16',
+            'date_debut' => date("Y-m-d h:i:s"),
+            'date_fin' => date("2021-7-21 07:12:54"),
+        ]);
+        DB::table('plannings')->insert([
+            'cours_id' => '15',
+            'date_debut' => date("Y-m-d h:i:s"),
+            'date_fin' => date("2021-7-21 07:12:54"),
+        ]);
+        DB::table('plannings')->insert([
+            'cours_id' => '13',
+            'date_debut' => date("Y-m-d h:i:s"),
+            'date_fin' => date("2021-7-21 07:12:54"),
+        ]);
+        DB::table('plannings')->insert([
+            'cours_id' => '14',
+            'date_debut' => date("Y-m-d h:i:s"),
+            'date_fin' => date("2021-7-21 07:12:54"),
+        ]);
+        DB::table('plannings')->insert([
+            'cours_id' => '12',
+            'date_debut' => date("Y-m-d h:i:s"),
+            'date_fin' => date("2021-7-21 07:12:54"),
+        ]);
+        DB::table('plannings')->insert([
+            'cours_id' => '11',
+            'date_debut' => date("Y-m-d h:i:s"),
+            'date_fin' => date("2021-7-21 07:12:54"),
         ]);
         DB::table('plannings')->insert([
             'cours_id' => '10',
